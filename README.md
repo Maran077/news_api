8caf2cc1b0aa147226a206fed13f4c0a381bb539
Maran077 <marvelmaran07@gmail.com>
2023-12-03 22:05:44 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# News API using Node.js with Web Scraping (Cheerio)
+
+This project is a News API built with Node.js that scrapes news from various categories such as anime, movies, series, games, and comics using Cheerio.
+
+## Features
+
+- Retrieves news articles from multiple categories.
+- Provides headline, description, image, and source for each article.
+- Easy-to-use API endpoints to fetch news data.
+
+## Usage
+Use the following endpoints to get news from different categories:
+
+GET /anime
+GET /movie
+GET /series
+GET /games
+GET /comic
+Each endpoint will return a list of news articles in JSON format, including headline, description, image, and source.
+
+## Dependencies
+Node.js
+Cheerio (for web scraping)
+Express (for server creation)
--- index.js
+++ index.js
@@ -3,77 +3,65 @@ const cheerio = require('cheerio')
 const axios = require('axios')
 const cors = require("cors")
 const app = express()
-app.use(cors({origin:"*"}))
+
+app.use(cors({ origin: "*" }))
 const PORT = process.env.PORT || 3000
-let news = []
-async function webscrapper(res,cat) {
-    try {
-        let url
-        switch(cat) {
-            case "anime":
-              url = `https://www.cbr.com/category/anime-news/`
-              break;
-            case "movie":
-              url = "https://www.cbr.com/category/movies/news-movies/"
-              break;
-            case "comic":
-              url = "https://www.cbr.com/category/comics/news/"
-              break;
-            case "series":
-              url = "https://www.cbr.com/category/tv/news-tv/"
-              break;
-            case "games":
-              url = "https://www.cbr.com/category/game-news/"
-              break;
-          }
-        const responce = await axios.get(url)
-        const html = responce.data
-        const $ = cheerio.load(html)
-<<<<<<< HEAD
-=======
-   
->>>>>>> a7d41df1da932cb8b897e1c083d35a57da38b441
-        $(".display-card.article.small", html).each(function(){
-          const image =  $(this).find('source').attr("srcset")
-          const heading = $(this).find("h5").text().replace(/\s\s+/g, '')
-          const para = $(this).find("p").text().replace(/\s\s+/g, '')
-          const source = "https://www.cbr.com/"
-          const one = {image,heading,para,source}
-          news = [...news,one]
 
-        })
+async function webscrapper(category,res) {
+  let news = []
 
-<<<<<<< HEAD
-=======
-        
->>>>>>> a7d41df1da932cb8b897e1c083d35a57da38b441
-        res.json(news)
-    } catch (error) {
-        console.log(error)
+  try {
+    let url
+    switch (category) {
+      case "anime":
+        url = `https://www.cbr.com/category/anime-news/`
+        break;
+      case "movie":
+        url = "https://www.cbr.com/category/movies/news-movies/"
+        break;
+      case "comic":
+        url = "https://www.cbr.com/category/comics/news/"
+        break;
+      case "series":
+        url = "https://www.cbr.com/category/tv/news-tv/"
+        break;
+      case "games":
+        url = "https://www.cbr.com/category/game-news/"
+        break;
+      default:
+        url = false
+        break;
     }
-    
+
+    const categoryNotFoundError = { error: "Category not found. Available categories is anime,movie,series,games,comic" }
+    if (!url) return res.json(categoryNotFoundError);
+
+    const responce = await axios.get(url)
+    const html = responce.data
+    const $ = cheerio.load(html)
+
+    $(".display-card.article.small", html).each(function () {
+      const image = $(this).find('source').attr("srcset")
+      const heading = $(this).find("h5").text().replace(/\s\s+/g, '')
+      const des = $(this).find("p").text().replace(/\s\s+/g, '')
+      const source = "https://www.cbr.com/"
+      const newNews = { image, heading, des, source }
+      news = [...news, newNews]
+
+    })
+
+    res.json(news)
+  } catch (error) {
+    res.send(error.name)
+  }
+
 }
-webscrapper()
-app.get('/anime', async (req,res)=>{
-    webscrapper(res,"anime")
-})
-app.get('/movie', async (req,res)=>{
-    webscrapper(res,"movie")
-})
-app.get('/comic', async (req,res)=>{
-    webscrapper(res,"comic")
-})
-app.get('/series', async (req,res)=>{
-    webscrapper(res,"series")
-})
-app.get('/games', async (req,res)=>{
-    webscrapper(res,"games")
+
+app.get('/:id', async (req, res) => {
+  const category = req.params.id
+  webscrapper(category ,res)
 })
 
-
-
-
-
-app.listen(PORT,()=>{
-    console.log("Online Now");
+app.listen(PORT, () => {
+  console.log("Online Now");
 })
